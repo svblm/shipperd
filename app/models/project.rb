179073093a8e6d3cd96291e36bf32734c3abadb2
JakeCataford <JakeCataford@Gmail.com>
2014-05-18 10:42:04 -0400

    Adds project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  validates :name, :description, :homepage, presence: true
+  validates :homepage, format: /https?:\/\/[\S]+/i
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "url validation for homepage" do
+    assert_difference ['Project.count'] do
+      Project.create(
+        name: 'test',
+        description: 'desc',
+        homepage: 'http://example.com'
+      )
+    end
+
+    assert_no_difference 'Project.count' do
+      Project.create(
+        name: 'no good',
+        description: 'horrible',
+        homepage: 'ftp:/ wow .coms'
+      )
+    end
+  end
 end
