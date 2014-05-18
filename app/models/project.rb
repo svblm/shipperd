1c8c383fcb4213b44d52fba2d2125ce238d93f5b
JakeCataford <JakeCataford@Gmail.com>
2014-05-18 11:15:28 -0400

    Adds Asset model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :assets
   has_and_belongs_to_many :users
   validates :name, :description, :homepage, presence: true
   validates :homepage, format: /https?:\/\/[\S]+/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140518145733) do
+ActiveRecord::Schema.define(version: 20140518150650) do
+
+  create_table "assets", force: true do |t|
+    t.string   "name"
+    t.integer  "project_id"
+    t.integer  "state",      default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "name"
