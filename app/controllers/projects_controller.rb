5a99f68e3692e777a0334c9e024d0bda9efabeda
Jake Cataford <jakecataford@gmail.com>
2014-06-09 11:11:00 -0400

    Don't stub current_user, instead set the session before each test.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  include ApplicationHelper
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,16 +3,11 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
   def setup
-    unless @controller.nil?
-      @controller.stubs(:current_user).returns(users(:user))
-    end
+
+    @controller.session[:user_id] = users(:user).id unless @controller.nil?
     @dropbox_mock = stub_everything
     files = []
     files.push "path" => "path/to/file.txt"
